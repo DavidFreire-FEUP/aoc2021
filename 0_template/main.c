79fc0e09e4c06975fdfe04b9803896fbff4299de
DavidFreire-FEUP <up201806711@fe.up.pt>
2021-12-12 10:41:20 +0000

    add template

---
+++ 0_template/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <assert.h>
+#include <math.h>
+
+typedef unsigned short int USINT;
+typedef unsigned int UINT;
+typedef unsigned long long int ULLI;
+
+#if 1
+#define INPUT_FILE "input.txt"
+#else
+#define INPUT_FILE "sample.txt"
+#endif 
+
+void part1()
+{
+    FILE *f;
+
+    f = fopen(INPUT_FILE,"r");
+    
+    
+    printf("Part 1: ");
+    return;
+}
+
+void part2()
+{
+    FILE *f;
+
+    f = fopen(INPUT_FILE,"r");
+    
+    printf("Part 2: \n");
+    return;
+}
+
+int main()
+{
+    part1();
+    part2();
+    return 0;
+}
